aa7b99c15ee042f396a3efca942fb0a77ff66ad2
fF <framef318@gmail.com>
2025-11-04 00:54:12 +0700

    Refactor PlayerData model fields: update defaults for position, armor, health, team, weapons, and clothes

--- project/apps/player/models.py
+++ project/apps/player/models.py
@@ -15,17 +15,17 @@ class PlayerData(BaseModel):
         related_name="data",
         verbose_name=_("player"),
     )
-    position = models.JSONField(default=dict, blank=True, null=True)
+    position = models.JSONField(default=[[-1969.4, 137.85, 27.69]], blank=True, null=True)
     rotation = models.FloatField(default=0, blank=True, null=True)
     skin = models.IntegerField(default=0, blank=True, null=True)
     interior = models.IntegerField(default=0, blank=True, null=True)
     dimension = models.IntegerField(default=0, blank=True, null=True)
-    team = models.CharField(max_length=50, blank=True, null=True)
-    health = models.IntegerField(default=0, blank=True, null=True)
+    team = models.CharField(default=0, max_length=50, blank=True, null=True)
+    health = models.IntegerField(default=100, blank=True, null=True)
     money = models.IntegerField(default=0, blank=True, null=True)
-    weapons = models.JSONField(default=dict, blank=True, null=True)
-    armor = models.FloatField(default=0, blank=True, null=True)
-    clothes = models.JSONField(default=dict, blank=True, null=True)
+    weapons = models.JSONField(default=[[]], blank=True, null=True)
+    armor = models.FloatField(default=100.00, blank=True, null=True)
+    clothes = models.JSONField(default=[[["vestblack", "vest", 0], ["player_face", "head", 1], ["jeansdenim", "jeans", 2], ["sneakerbincblk", "sneaker", 3]]], blank=True, null=True)
     wantedlevel = models.IntegerField(default=0, blank=True, null=True)
 
     class Meta:
