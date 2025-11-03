0e39e9d62b5d8f7987a4bffeaca975aac0245ccd
fF <framef318@gmail.com>
2025-11-03 13:05:41 +0700

    Remove unused schemas and dependencies related to Django Ninja from the player app. Clean up URLs and settings to reflect the removal of NinjaAPI and its associated components.

--- project/apps/player/urls.py
+++ project/apps/player/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from ninja import NinjaAPI
-from apps.player.api import router as player_router
+from apps.player.api import *
 
-api = NinjaAPI()
-
-# Register routers
-api.add_router("/player/", player_router)
 
 urlpatterns = [
-    path("", api.urls),
+
 ]
