61173849445de1942c787a1b9b67a19625d9de7d
fF <framef318@gmail.com>
2025-11-03 10:27:15 +0700

    Refactor settings and URL configuration for consistency and readability

--- project/config/urls.py
+++ project/config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
